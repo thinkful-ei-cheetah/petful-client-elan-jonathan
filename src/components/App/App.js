2ccd3c2b3fbb27ab7c6acbc381de518ca377b06e
elangreen <green.elan5@gmail.com>
2019-06-21 13:34:41 -0700

    conditional rendering on line count

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 import Nav from '../Nav/Nav'
 import './App.css'
 import HomePage from '../HomePage/HomePage'
@@ -30,15 +31,57 @@ class App extends Component {
         story: "Owner Passed away"
       }
     ],
-    users: [
-      {
-        id: 1,
-        enterTime: Date.now()
-      }
-    ]
+    users: [],
+    currentUser: '',
+    isAdopting: false
   }
+
+  async componentDidMount() {
+    const response = await axios.get('http://localhost:8080/api/user')
+    const users = await response.data
+    this.setState({
+      users: [users]
+    })
+    setInterval(async () => {
+      const response = await axios.get('http://localhost:8080/api/user')
+      const users = await response.data
+      this.setState({
+        users: [users]
+      })
+    }, 300000)
+  }
+
+  handleAddToAdoptQueue = async () => {
+    const response = await axios.post('http://localhost:8080/api/user')
+    const user = await response.data
+    this.setState({ isAdopting: true, currentUser: user.currentUser })
+    console.log(user) 
+  }
+
+  handleAdoptCat = async () => {
+    const { currentUser } = this.state
+    const { users } = this.state
+    if(currentUser === users[0].nextinline.data.entertime) {
+      axios.delete('http://localhost:8080/api/cat')
+      axios.delete('http://localhost:8080/api/user', {
+        entertime: currentUser
+      })
+    }
+  }
+
+  handleAdoptDog = async () => {
+    const { currentUser } = this.state
+    const { users } = this.state
+    if(currentUser === users[0].nextinline.data.entertime) {
+      axios.delete('http://localhost:8080/api/dog')
+      axios.delete('http://localhost:8080/api/user', {
+        entertime: currentUser
+      })
+    }
+  }
+
   render() {
-    const { cats, dogs } = this.state
+    const { cats, dogs, users } = this.state
     return (
       <div className="App">
         <nav className="nav-bar" role="navigation">
@@ -49,7 +92,14 @@ class App extends Component {
             <Route
               exact path={"/"}
               render={(routeProps) => (
-                <HomePage {...routeProps} cats={cats} dogs={dogs} />
+                <HomePage 
+                  {...routeProps} 
+                  cats={cats} 
+                  dogs={dogs} 
+                  users={users} 
+                  handleClick={this.handleAddToAdoptQueue} 
+                  isAdopting={this.state.isAdopting}
+                />
               )}
             />
           </Switch>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
 import AnimalInfo from '../AnimalInfo/AnimalInfo';
+import axios from 'axios'
 import Button from '../Button/Button';
 
 
@@ -11,14 +12,32 @@ export class HomePage extends Component {
     handleClick: () => {}
   }
   
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      users: {}
+    }
+  }
+
+  async componentWillMount() {
+    const response = await axios.get('http://localhost:8080/api/user')
+    const users = await response.data
+    console.log(users)
+    this.setState({ users: users })
+  }
+  
   render() {
-    let { cats, dogs } = this.props
+    const { users } = this.state
+    let { cats, dogs, isAdopting } = this.props
     cats = cats[0]
     dogs = dogs[0]
+    console.log(users)
     return (
       <div className="home-page">
-        <Header />
-        <Button text={'Click here to start adopting!'} handleClick={this.props.handleClick} />
+        <Header /> 
+        {users.count === 1 ? `It's your turn to adopt!` : `${users.count} in line ahead of you.`}
+        {!isAdopting && <Button text={'Click here to start adopting!'} handleClick={this.props.handleClick} />}
         <AnimalInfo 
           id={dogs.id} 
           name={dogs.name} 
